4e949e2aa26c7c0d02954a14c4909a435067c99f
hrbrmstr <bob@rud.is>
2018-11-14 08:01:13 -0500

    tweaked pdf_info

--- R/metadata.R
+++ R/metadata.R
@@ -28,13 +28,13 @@ pdf_info <- function(path) {
       fields <- sapply(.jevalArray(ret$getMetadataKeys()$toArray()), function(x) x$toString())
 
       data.frame(
-        title = ret$getTitle(),
-        subject = ret$getSubject(),
-        author = ret$getAuthor(),
-        creation_date = ret$getCreationDate()$getTime()$toLocaleString(),
-        modification_date = ret$getModificationDate()$getTime()$toLocaleString(),
-        producer = ret$getProducer(),
-        keywords = ret$getKeywords(),
+        title = ret$getTitle() %||% NA_character_,
+        subject = ret$getSubject() %||% NA_character_,
+        author = ret$getAuthor() %||% NA_character_,
+        creation_date = ret$getCreationDate()$getTime()$toLocaleString() %||% NA_character_,
+        modification_date = ret$getModificationDate()$getTime()$toLocaleString() %||% NA_character_,
+        producer = ret$getProducer() %||% NA_character_,
+        keywords = ret$getKeywords() %||% NA_character_,
 #        custom_metadata = ret$getCustomMetadataValue(),
         stringsAsFactors = FALSE
       ) -> tmp
