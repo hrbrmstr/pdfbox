b7a6c685b1a3f1065e03cf7734b5f16aaa443317
hrbrmstr <bob@rud.is>
2019-01-15 12:54:25 -0500

    travis; passes CRAN

--- R/logging.R
+++ R/logging.R
@@ -1,21 +1,23 @@
-#' @title rJava logging
-#' @description Toggle verbose rJava logging
-#' @details This function turns off the somewhat verbose rJava logging, most of which is
-#'          uninformative. It is called automatically when tabulizer is attached via
-#'          \code{library()}, \code{require}, etc. To keep logging on, load the package
-#'          namespace using \code{requireNamespace("tabulizer")} and reference functions
-#'          in using fully qualified references (e.g., \code{tabulizer::extract_tables()}.
+#' Stop logging
+#'
+#' Toggle verbose rJava logging
+#'
+#' This function turns off the somewhat verbose rJava logging, most of which is
+#' uninformative. It is called automatically when tabulizer is attached via
+#' \code{library()}, \code{require}, etc. To keep logging on, load the package
+#' namespace using \code{requireNamespace("pdfbox")} and reference functions
+#' in using fully qualified references.
+#'
+#' @md
 #' @note This resets a global Java setting and may affect logging of other rJava operations,
 #'       requiring a restart of R.
 #' @return `NULL`, invisibly.
 #' @author Thomas J. Leeper <thosjleeper@@gmail.com>
-#' @noRd
-#' @md
-#' @examples
-#' \dontrun{
+#' @keywords internal
+#' @export
+#' @examples \dontrun{
 #' stop_logging()
 #' }
-#' @export
 stop_logging <- function() {
   rJava::J("java.util.logging.LogManager")$getLogManager()$reset()
   invisible(NULL)
